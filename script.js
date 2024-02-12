b976a75775c6621dbef9e38dd6bb55f694f710dd
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2024-02-12 18:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const form = document.getElementById('urlForm');
+const urlText = document.getElementById('url');
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault(); // Prevent form submission
+
+	// Get input field values
+	const name = document.getElementById('name').value;
+	const year = document.getElementById('year').value;
+
+	// Update url text with query string
+	urlText.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+});
+
+
